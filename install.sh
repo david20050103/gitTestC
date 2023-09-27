011dd0e29c2abd393b04df6d0a9fee172468a45e
chenchanglee <92417020+chenchanglee@users.noreply.github.com>
2023-09-27 17:59:33 +0800

    bu->fcos.yml

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-podman run --interactive --rm quay.io/coreos/butane:release --pretty --strict < your_config.bu > container.ign
+podman run --interactive --rm quay.io/coreos/butane:release --pretty --strict < fcos.yml > container.ign
 sudo coreos-installer install /dev/sda --ignition-file container.ign --copy-network  --insecure-ignition
